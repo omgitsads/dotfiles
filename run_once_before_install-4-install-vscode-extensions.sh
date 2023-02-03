0d3bf98ebe14ff78550f710b394d8909d6616154
Adam Holt <me@adamholt.co.uk>
2023-02-03 17:07:18 +0100

    Add system config and VSCode extensions

---
+++ run_once_before_install-4-install-vscode-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension bungcip.better-toml
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.copilot
+code --install-extension golang.go
+code --install-extension GraphQL.vscode-graphql
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension vscodevim.vim
