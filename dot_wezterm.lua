fba5563e254c37c4154f2cfb333b1c27bff4a17e
Adam Holt <me@adamholt.co.uk>
2024-08-09 10:47:22 +0200

    add window management

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,48 +9,73 @@ c.window_decorations = "RESIZE"
 
 c.color_scheme = "catppuccin-frappe"
 c.window_background_opacity = 0.9
-c.macos_window_background_blur = 30
+c.macos_window_background_blur = 100
 
 c.inactive_pane_hsb = {
 	saturation = 0.9,
 	brightness = 0.8,
 }
 
+c.leader = { key = "a", mods = "CTRL" }
+c.keys = {
+	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "v",
+		mods = "LEADER",
+		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "LeftArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "DownArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "UpArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "RightArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "LeftArrow", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "DownArrow", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "UpArrow", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	{ key = "RightArrow", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+}
+
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(c)
-
--- c.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
--- c.keys = {
--- 	{
--- 		key = "\\",
--- 		mods = "LEADER",
--- 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
--- 	},
--- 	{
--- 		key = "|",
--- 		mods = "LEADER|SHIFT",
--- 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
--- 	},
--- 	{
--- 		key = "a",
--- 		mods = "LEADER|CTRL",
--- 		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
--- 	},
--- }
-
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-smart_splits.apply_to_config(c, {
-	-- the default config is here, if you'd like to use the default keys,
-	-- you can omit this configuration table parameter and just use
-	-- smart_splits.apply_to_config(config)
-
-	-- directional keys to use in order of: left, down, up, right
-	direction_keys = { "h", "j", "k", "l" },
-	-- modifier keys to combine with direction_keys
-	modifiers = {
-		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
-		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
-	},
-})
 
 return c
